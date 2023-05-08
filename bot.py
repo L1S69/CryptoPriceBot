08c5645a99bb51881daf88d4d06c4a47a867aa33
Pavel <89720804+L1S69@users.noreply.github.com>
2023-05-08 22:00:23 +0600

    Added comments

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
-import requests
-from pycoingecko import CoinGeckoAPI
-import telebot
+# Import required libraries
+import requests # for making HTTP requests
+from pycoingecko import CoinGeckoAPI # for fetching crypto price data
+import telebot # for creating Telegram bot
 
+# Telegram bot token obtained from BotFather
 token = "**********************************************"
+# Initialize the bot with the token
 qr_gen_bot = telebot.TeleBot(token)
 
 keyboard = telebot.types.ReplyKeyboardMarkup(row_width=3)
 
+# Create custom keyboard with cryptocurrency buttons
 button_btc = telebot.types.KeyboardButton('BTC')
 button_eth = telebot.types.KeyboardButton('ETH')
 button_ltc = telebot.types.KeyboardButton('LTC')
@@ -20,28 +24,38 @@ keyboard.add(button_btc, button_eth, button_ltc,
              button_xrp, button_bch, button_doge,
              button_ada, button_dot, button_link)
 
+# Initialize CoinGeckoAPI object
 cg = CoinGeckoAPI()
 
+# Function to fetch the cryptocurrency price
 def get_crypto_price(coin):
-    id = cg.search(coin)["coins"][0]["id"]
-    crypto_data = cg.get_price(ids=id, vs_currencies='usd')
-    price = crypto_data[id]['usd']
-    return [id, price]
+    id = cg.search(coin)["coins"][0]["id"] # Search the coin on CoinGecko API and get its ID
+    
+    crypto_data = cg.get_price(ids=id, vs_currencies='usd') # Fetch the price data for the specified cryptocurrency
+    price = crypto_data[id]['usd'] # Get the USD price for the specified cryptocurrency
+    
+    return [id, price] # Return the cryptocurrency ID and USD price in a list
 
-
+# Handler for /start and /help commands
 @qr_gen_bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
+    # Send welcome message with custom keyboard
     qr_gen_bot.send_message(message.chat.id,
                             f"Hello, {message.from_user.first_name}, what cryptocurrency price are you interested in?",
                             reply_markup=keyboard)
 
+# Handler for text messages received from users
 @qr_gen_bot.message_handler(content_types=["text"])
 def get_text_messages(message):
-    text = message.text
-    data = get_crypto_price(text)
+    text = message.text # Get the text message from the user
+    data = get_crypto_price(text) # Call get_crypto_price function to get the cryptocurrency data
+    
+    # Extract cryptocurrency ID and USD price from the data
     coin = data[0]
     price = data[1]
+    
+    # Send the cryptocurrency price to the user
     qr_gen_bot.send_message(message.chat.id, f"The price of {coin} is ${price:.2f}", reply_markup=keyboard)
 
-
+# Start the bot
 qr_gen_bot.infinity_polling()
