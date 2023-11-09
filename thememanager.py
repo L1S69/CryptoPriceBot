b7c10ac908c41cc4901a3e3d2ce8cbba33fd23b2
L1S69 <89720804+L1S69@users.noreply.github.com>
2023-11-09 08:38:50 +0000

    Added more themes

--- thememanager.py
+++ thememanager.py
@@ -5,12 +5,11 @@ import dbmanager
 themes = dict()
 
 for file in os.listdir("themes/"):
-    with open(f"themes/{file}", "r") as f:  # Open each file in read mode
-        themes[file[:-5]] = json.loads(
-            f.read())  # Store themes in the dictionary using file name as the key
+  with open(f"themes/{file}", "r") as f:  # Open each file in read mode
+    themes[file[:-5]] = json.loads(
+        f.read())  # Store themes in the dictionary using file name as the key
 
 
 def get_theme(chat_id: int):
-    theme_id = dbmanager.get_data(chat_id, "theme",
-                                  "catppuccin-mocha")
-    return themes[theme_id]
+  theme_id = dbmanager.get_data(chat_id, "theme", "standard-dark")
+  return themes[theme_id]
